46397dbf0fa29146ea81513deaa90369c2a982f8
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-09-08 02:44:44 +0800

    fate: add InsertNotYetFashion

--- Creational Pattern/Object Pool/Object-Pool-Example/main.go
+++ Creational Pattern/Object Pool/Object-Pool-Example/main.go
@@ -14,7 +14,13 @@ func main() {
 	})
 
 	labtopPool.AddToFashionList()
+	labtopPool.AddToFashionList()
 
+	fmt.Println("OnFashion: ", labtopPool.OnFashion[0].Name, " Length: ", len(labtopPool.OnFashion))
+	fmt.Println("OldFashion: ", labtopPool.OldFashion[0].Name, " Length: ", len(labtopPool.OldFashion))
+	fmt.Println("NotYetFashion: ", labtopPool.NotYetFashion[0].Name, " Length: ", len(labtopPool.NotYetFashion))
+
+	labtopPool.InsertNotYetFashion(&Pool.Labtop{Name: "Khoa's Labtop"})
 	labtopPool.AddToFashionList()
 
 	fmt.Println("OnFashion: ", labtopPool.OnFashion[0].Name, " Length: ", len(labtopPool.OnFashion))
